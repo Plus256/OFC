aa642916bbf300abd6e9397d0813b2071b2b372c
Collins Wagaba <wagabaplus256@gmail.com>
2015-08-09 18:34:38 +0300

    importing project files

---
+++ inc/cnf.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+session_start();
+/*local credentials*/
+$host="127.0.0.1";
+$db="buwooya";
+$user="root";
+$pwd="root";
+$port=3306;
+$conn=mysqli_connect($host, $user, $pwd, $db, $port) or die(mysqli_error());
+/*end of local*/
+
+/*remote credentials*
+$host="127.0.0.1";
+$db="plus256buwooya";
+$user="plus256dbadmin";
+$pwd="Psalm_23*";
+$port=3306;
+$conn=mysqli_connect($host, $user, $pwd, $db, $port) or die(mysqli_error());
+/*end of remote*/
+$short_name="OFC";
+$full_name="Our Forgotten Children";
+$logo="gra/logo.png";
+$noscript="Enable JavaScript in your browser to have the best experience at ".$short_name;
+?>
---
+++ inc/fot.php
@@ -0,0 +1,26 @@
+<div id="footer">
+	<div class="wrapper">
+        <div id="fot_soc">
+            <a href="#" target="_NEW"><img src="gra/facebook.png" /></a>
+            <a href="#" target="_NEW"><img src="gra/twitter.png" /></a>
+            <a href="#" target="_NEW"><img src="gra/instagram.png" /></a>
+            <div class="spacer"></div>
+        </div>
+    	<div id="fot_links">
+            <a href="./#love" id="about_fot_link">Love</a>
+            <a href="./#play" id="contact_fot_link">Play</a>
+            <a href="./#donate" id="ad_fot_link">Donate</a>
+            <a href="./#contact" id="contr_fot_link">Contact</a>
+						<a href="./#about" id="contr_fot_link">About</a>
+            <div class="spacer"></div>
+        </div>
+        <div id="copyright">Copyright &copy; <?php echo date('Y')." <a href='./'>".$full_name."</a>"; ?></div>
+    	<div class="spacer"></div>
+    </div>
+    <div id="fb-root"></div>
+</div>
+</body>
+</html>
+<?php
+ob_end_flush();
+?>
