da5123987c46b4985cad1c6b81328737ce728106
Collins Wagaba <wagabaplus256@gmail.com>
2015-09-15 15:52:14 +0300

    type kit

--- inc/hed.php
+++ inc/hed.php
@@ -17,8 +17,16 @@ require_once("inc/fun.php");
 <!--Plus256 Network, Ltd-->
 <!--www.plus256.com-->
 <script type="text/javascript" src="js/fun.js"></script>
-<script src="//use.typekit.net/nww6oyv.js"></script>
-<script>try{Typekit.load();}catch(e){}</script>
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'bky8qig',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
 </head>
 <body>
 <noscript id="noscript"><?php echo $noscript; ?></noscript>
